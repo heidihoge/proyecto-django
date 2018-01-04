bbab1e1f545d5e8d253dba522e87428ab63781fe
heidihoge <heidihoge93@gmail.com>
2018-01-04 19:41:52 -0300

    Template-Form-Views

--- src/aquasoft/admin.py
+++ src/aquasoft/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Curso
 
 # Register your models here.
+class AdminCurso(admin.ModelAdmin):
+    list_display = ["__str__", "date","descripcion"]
+    list_display_links = ["date" ]
+    list_filter = ["nombre_curso"]
+    list_editable = ["descripcion"]
+    search_fields =  ["nombre_curso"]
+    class Meta:
+        model = Curso
+
+admin.site.register(Curso,AdminCurso)
--- src/aquasoft/models.py
+++ src/aquasoft/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+import time
+from datetime import date
 # Create your models here.
+class Curso(models.Model):
+    nombre_curso = models.CharField(max_length=100, blank=True,null=False)
+    descripcion = models.CharField (max_length=100, blank=True,null=False)
+    date = date.today()
+
+    def __str__(self):
+        return self.nombre_curso
+
