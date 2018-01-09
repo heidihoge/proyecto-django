18a4f16e5b207cfb696ffbf371e42f3bc363e21f
heidihoge <heidihoge93@gmail.com>
2018-01-08 21:05:30 -0300

    validaciones de formulario

--- src/aquasoft/views.py
+++ src/aquasoft/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from .forms import RegForm
 # Create your views here.
 def inicio(request):
-    form = RegForm()
+    form = RegForm(request.POST or None)
+    if form.is_valid():
+        form_data = form.cleaned_data
+        print(form_data.get("nombre"))
+        print(form_data.get("edad"))
     context = {
         "form" : form
     }
