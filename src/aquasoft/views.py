d0b3a0be60255d11047cdbbf5bc854369744b3c1
heidihoge <heidihoge93@gmail.com>
2018-01-11 22:24:36 -0300

    Contexto en vista, contexto en plantilla

--- src/aquasoft/views.py
+++ src/aquasoft/views.py
@@ -4,12 +4,14 @@ from .models import Curso
 # Create your views here.
 def inicio(request):
     form = RegForm(request.POST or None)
+    titulo = "Hola"
     if form.is_valid():
         form_data = form.cleaned_data
         nombre_cur= form_data.get("nombre_curso")
         desc_cur = form_data.get("descripcion")
         obj = Curso.objects.create(nombre_curso = nombre_cur, descripcion= desc_cur)
     context = {
+        "titulo" : titulo,
         "form" : form
     }
     return render(request,"inicio.html",context)
