ce6d418829d80cc7750dfb7f284e5762586f9e88
heidihoge <heidihoge93@gmail.com>
2018-01-11 21:54:36 -0300

    Validacion de dato de formulario

--- src/aquasoft/forms.py
+++ src/aquasoft/forms.py
@@ -1,13 +1,16 @@
 from django import forms
-from datetime import date
+
 from .models import Curso
 
 class RegModelForm(forms.ModelForm):
     class Meta:
         model = Curso
         fields = ["nombre_curso" , "descripcion"]
-
-
+    def clean_nombre_curso(self):
+        curso = self.cleaned_data.get("nombre_curso")
+        if not "natacion" in curso:
+            raise forms.ValidationError("Por favor ingresa un curso que sea de Natacion")
+        return curso
 class RegForm(forms.Form):
     nombre_curso = forms.CharField(max_length=100)
     descripcion = forms.CharField(max_length=100)
