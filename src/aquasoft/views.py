5d736cd87ba3e957f6c753b595b4bed1bc332268
heidihoge <heidihoge93@gmail.com>
2018-01-28 16:43:09 -0300

    Menu principal de la aplicacion

--- src/aquasoft/views.py
+++ src/aquasoft/views.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from .forms import ContactForm ,RegModelForm
 from .models import Curso
+
 # Create your views here.
 def inicio(request):
     form = RegModelForm(request.POST or None)
